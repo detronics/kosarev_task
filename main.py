8a43315af360b319bdd870e2257ff23037f48dd1
Konstantin <detronics52@gmail.com>
2022-04-15 14:34:38 +0300

    Накатил нужные библиотеки, проверил подключение к БД

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
+import psycopg2
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
 
-print('hello')
+
+app = QApplication(sys.argv)
+
+w = QWidget()
+w.resize(250, 150)
+w.move(550, 300)
+w.setWindowTitle('Simple')
+w.show()
+
+sys.exit(app.exec_())
+
+# try:
+#     con = psycopg2.connect(
+#         database="staff_db",
+#         user="postgres",
+#         password="123456",
+#         host="127.0.0.1",
+#         port="5432"
+#     )
+#     cursor = con.cursor()
+#     # Распечатать сведения о PostgreSQL
+#     print("Информация о сервере PostgreSQL")
+#     print(con.get_dsn_parameters(), "\n")
+#     # Выполнение SQL-запроса
+#     cursor.execute("SELECT version();")
+#     # Получить результат
+#     record = cursor.fetchone()
+#     print("Вы подключены к - ", record, "\n")
+# except (Exception) as error:
+#     print("Ошибка при работе с PostgreSQL", error)
+
+
+
+
+
